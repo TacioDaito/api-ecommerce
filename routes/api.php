9ac034f3f9ea19df4145fa59f660726978523100
TacioDaito <taciokikuchi@gmail.com>
2025-05-12 22:29:48 -0300

    Passport and API setup; User seeding

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:api');
+
+Route::get('login', function () {
+    return response()->json(['message' => 'Login successful']);
+});
