0455adf7b77b3fa42fe29fda08c77d37029272fb
TacioDaito <taciokikuchi@gmail.com>
2025-05-23 22:19:11 -0300

    Policy responses

--- app/Policies/AdminPolicy.php
+++ app/Policies/AdminPolicy.php
@@ -1,11 +1,14 @@
 <?php
 namespace App\Policies;
 use App\Models\User;
+use Illuminate\Auth\Access\Response;
 
 class AdminPolicy
 {
-    public function onlyAllowAdmin(User $user): bool
+    public function onlyAllowAdmin(User $user): Response
     {
-        return $user->role->name === 'admin';
+        return $user->role->name === 'admin' ?
+        Response::allow() :
+        Response::denyWithStatus(404, 'Route not found.');
     }
 }
--- app/Policies/OrderPolicy.php
+++ app/Policies/OrderPolicy.php
@@ -1,19 +1,22 @@
 <?php
-
 namespace App\Policies;
-
 use App\Models\Order;
 use App\Models\User;
+use Illuminate\Auth\Access\Response;
 
 class OrderPolicy
 {
-    public function accessOrder(User $user, Order $order): bool
+    public function accessOrder(User $user, Order $order): Response
     {
-        return $user->id === $order->user_id;
+        return $user->id === $order->user_id ?
+        Response::allow() :
+        Response::denyWithStatus(404, 'Order not found.');
     }
 
-    public function createOrder(User $user, $request_user_id): bool
+    public function createOrder(User $user, $request_user_id): Response
     {
-        return $user->id === $request_user_id;
+        return $user->id === $request_user_id ?
+        Response::allow() :
+        Response::deny('You do not have permission to create this order.');
     }
 }
