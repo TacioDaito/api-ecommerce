9a997176017b21adfd2c2148df13a7cc0a532268
TacioDaito <taciokikuchi@gmail.com>
2025-06-12 14:11:22 -0300

    N+1 query problem solution

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,7 +48,7 @@ class User extends Authenticatable
 
     public function orders(): hasMany
     {
-        return $this->hasMany(Order::class);
+        return $this->hasMany(Order::class)->chaperone();
     }
 
     public function role(): BelongsTo
